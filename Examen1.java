f9575b1dc20d2f2093d45abb2988954dce14d50b
1JERRY12 <143622260+1JERRY12@users.noreply.github.com>
2023-09-20 09:02:34 -0600

    Add files via upload

---
+++ Examen1.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Examen1 {
+  
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("ingrese el nombre del instrumento1");
+        String N1= Scanner.nextLine();
+        
+        System.out.println("ingrese el precio del instrumento1");
+        String P1 = Scanner.nextLine();
+        
+        System.out.println("ingrese el nombre del instrumento2");
+        String N2= Scanner.nextLine();
+        
+        System.out.println("ingrese el precio del instrumento2");
+        String P2= Scanner.nextLine();
+        
+        System.out.println("ingrese el nombre del instrumento3");
+        String N3= Scanner.nextLine();
+        
+        System.out.println("ingrese el precio del instrumento3");
+        String P3= Scanner.nextLine();
+        
+        System.out.println("introducir nombre del usuario");
+        String N4= Scanner.nextLine();
+        
+        System.out.println("precio de la compra");
+        String P4= Scanner.nextLine();
+        
+        System.out.println("precio de la compra");
+        String N5= Scanner.nextLine();
+             
+        double instrumento1 = (1250.45-P1);
+        double Producto2 = (1250.45-P2);
+        double Producto3 = (1250.45-P3);
+        
+        Scanner.close;
+        
+        double producto1 = (productop1/n1)*100
+        double producto2 = (productop1/n1)
+        double producto3 = (productop1/n1)*100
+                
+        
+    }
+    
+}
